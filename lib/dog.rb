0f3b7d37dbc0674cdcd4ead560b51a57263c3a43
A Glazer <avigayilglazer@gmail.com>
2019-01-28 19:48:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+end
+
+fido = Dog.new("Fido")
+fido.name
+
+fido.name = "New Fido"
+fido.name
