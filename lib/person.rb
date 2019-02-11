584659fd6d0a6444f7013626c8baea49c32bbc15
David Acosta <davidacosta25081@yahoo.com>
2019-02-11 06:47:45 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,33 @@
+class Person
+
+def name = (new_name)
+    @name = new_name
+  end
+
+
+def name
+  @name
+end
+
+
+
+
+def job = (new_job)
+  @job = new_job
+end
+
+def job
+  @job
+
+end
+
+
+
+end
+
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+
+beyonce = Person.new
+beyonce.job = "Singer"
